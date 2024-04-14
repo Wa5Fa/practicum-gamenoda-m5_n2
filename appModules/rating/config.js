8ad7d2bdd50a8573d8d2fdda1888570ab2f39d36
Wa5Fa <139921639+wa5fa@users.noreply.github.com>
2024-04-14 17:51:02 +0300

    🔧patch: update config.js

--- appModules/rating/config.js
+++ appModules/rating/config.js
@@ -1,5 +1,5 @@
 // const PATH_TO_RATING_FILE = "./dataset/rating.json";
-const PATH_TO_RATING_FILE = "/Users/user/Documents/lexpearson-module_5/lexpearson-module_5-n2-gamerating/dataset/rating.json";
+const PATH_TO_RATING_FILE = "./dataset/rating.json";
 const WEIGHT = {
 	gameplay: 2,
 	design: 1,
